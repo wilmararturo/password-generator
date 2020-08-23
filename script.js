b4499c9baca9e690c1f1a4eb7dc4212d87efbd78
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-08-23 02:36:41 -0400

    added comments and fixed validation

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Assignment Code
+var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 var passwordFormLength = document.querySelector("#passwordLength");
 var lowecaseSwitch = document.querySelector("#lowercaseSwitch");
@@ -6,6 +7,7 @@ var uppercaseSwitch = document.querySelector("#uppercaseSwitch");
 var numericSwitch = document.querySelector("#numericSwitch");
 var specialCharSwitch = document.querySelector("#specialCharSwitch");
 
+// An object for the password options
 var passwordOptions = {
   passwordLength: 8,
   includeLowerCase: false,
@@ -14,12 +16,13 @@ var passwordOptions = {
   includeSpecialChars: false,
 }
 
-
+// strings for the different character classes
 var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var numbers = "0123456789";
 
+// update the password options object based on selections
 function updatePasswordOptions() {
   console.log(event.target.id);
   if (event.target.id === "lowercaseSwitch") {
@@ -37,18 +40,27 @@ function updatePasswordOptions() {
 }
 
 
-
+// Check the password options to meet compliance 
+// return true if in compliance
 function validatePasswordOptions() {
+  //length must be between 8 and 128 else raise an alert
   if (passwordOptions.passwordLength < 8 || passwordOptions.passwordLength > 128) {
     alert("The Password Lenth must be between 8 and 128");
+    return false;
   }
 
+  // one of these must be true else raise an alert
   if (passwordOptions.includeLowerCase === false && passwordOptions.includeUpperCase === false && passwordOptions.includeNumbers === false && passwordOptions.includeSpecialChars === false) {
     alert("Please choose at lease one of lowercase, uppercase, numeric, and/or special characters");
+    return false;
   }
+  return true;
+
 }
-// Generate password
+
+// Generate password from the selected options
 function generatePassword() {
+  //empty vars for the selected chars and new password
   var allChars = "";
   var newPassword = "";
 
@@ -65,9 +77,13 @@ function generatePassword() {
     allChars += specialChars;
   }
   console.log(allChars);
+  console.log(allChars.length);
 
-  allCharsArray = allChars.split("");
+  // make the character list an array to pull elements at random
+  var allCharsArray = allChars.split("");
+  // loop through the char array until the required number of chars are picked
   for (i = 0; i < passwordOptions.passwordLength; i++) {
+    // random number stuff
     var allCharsIndex = Math.floor(Math.random() * allChars.length);
     newPassword += allCharsArray[allCharsIndex];
     console.log(newPassword);
@@ -77,10 +93,15 @@ function generatePassword() {
 }
 // Write password to the #password input
 function writePassword() {
-  validatePasswordOptions();
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.textContent = password;
+  var optionsPassed = validatePasswordOptions();
+  console.log(optionsPassed);
+  //only generate a password if the options are valid
+  if (validatePasswordOptions()) {
+
+    var password = generatePassword();
+    passwordText.textContent = password;
+
+  }
 
 }
 
