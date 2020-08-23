7f8d153377966281fd86ac0957d92e3ed97230af
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-08-22 23:56:36 -0400

    working on options and generation

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowecaseSwitch = document.querySelector("#lowercaseSwitch");
+var uppercaseSwitch = document.querySelector("#uppercaseSwitch");
+var numericSwitch = document.querySelector("#numericSwitch");
+var specialCharSwitch = document.querySelector("#specialCharSwitch");
+
 var passwordOptions = {
-  passwordLength: 0,
+  passwordLength: document.querySelector("#passwordLength").value,
   includeLowerCase: false,
   includeUpperCase: false,
   includeNumbers: false,
   includeSpecialChars: false,
 }
 
+
 var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 var numbers = "0123456789";
 
-function getPasswordOptions() {
-  //prompt for length
-  while (passwordOptions.passwordLength < 8 || passwordOptions.passwordLength > 128) {
-    var enteredLength = parseInt(prompt("Please enter a length for the password between 8 and 128"));
-    if (Number.isInteger(enteredLength)) {
-      passwordOptions.passwordLength = enteredLength;
-    }
-
+function updateLowercaseOption() {
+  if (passwordOptions.includeLowerCase === false) {
+    passwordOptions.includeLowerCase = true;
   }
+  else {
+    passwordOptions.includeLowerCase = true;
+  }
+}
+
+function updateUppercaseOption() {
+  if (passwordOptions.includeUpperCase === false) {
+    passwordOptions.includeUpperCase = true;
+  }
+  else {
+    passwordOptions.includeUpperCase = true;
+  }
+}
+
+function updateNumericOption() {
+  if (passwordOptions.includeNumbers === false) {
+    passwordOptions.includeNumbers = true;
+  }
+  else {
+    passwordOptions.includeNumbers = true;
+  }
+}
+
+function updateLowercaseOption() {
+  if (passwordOptions.includeLowerCase === false) {
+    passwordOptions.includeLowerCase = true;
+  }
+  else {
+    passwordOptions.includeLowerCase = true;
+  }
+}
+
+function getPasswordOptions() {
+  event.preventDefault();
+  console.log(passwordOptions);
+  //validate form entries
+
+
 
   //prompt for options "lowercase, uppercase, numeric, and/or special characters"
 }
@@ -42,4 +81,9 @@ function writePassword() {
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+lowecaseSwitch.addEventListener("click", updateLowercaseOption);
+uppercaseSwitch.addEventListener("click", updateUppercaseOption);
+numericSwitch.addEventListener("click", updateNumericOption);
+specialCharSwitch.addEventListener("click", updateSpecialCharOption);
+generateBtn.addEventListener("click", getPasswordOptions);
+
